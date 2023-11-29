77b5b85e11d73c5150f4664bc8c8d6df3386e857
Jota Díaz <josmondia2@alu.edu.gva.es>
2023-11-29 23:27:52 +0100

    Cambios Jota

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       String nombre;
+       String nombre, ciudad;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("¿Cómo te llamas? ");
